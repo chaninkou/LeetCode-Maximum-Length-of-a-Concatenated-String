99127e92d95a831e17dac35e88abf5a4fd1051f5
chaninkou <michaelkoulove@yahoo.com>
2020-05-24 00:06:05 -0700

    Great problem on getting the max length unique words.

--- src/leetcode1239/FindMaximumLength.java
+++ src/leetcode1239/FindMaximumLength.java
@@ -7,15 +7,18 @@ public class FindMaximumLength {
     public int maxLength(List<String> arr) {
         List<String> result = new ArrayList<>();
         
+        // As a base case to add itself
         result.add("");
         
         for(String word : arr){
+        	// Input could have duplicates too
             if(!isUnique(word)){
                 continue;
             }
             
             List<String> tempList = new ArrayList<>();
             
+            // For every combination, save the unique one
             for(String savedWord : result){
                 String temp = savedWord + word;
                 
@@ -36,7 +39,9 @@ public class FindMaximumLength {
         return maxLength;
     }
     
+    // A helper method that check if duplicates
     private boolean isUnique(String word){
+    	// greater than 26 means it has duplicates
         if(word.length() > 26){
             return false;
         }
--- src/leetcode1239/Main.java
+++ src/leetcode1239/Main.java
@@ -1,7 +1,22 @@
 package leetcode1239;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
+		List<String> input = new ArrayList<>();
+		
+		input.add("un");
+		input.add("iq");
+		input.add("ue");
+		
+		System.out.println(input);
+		
+		FindMaximumLength solution = new FindMaximumLength();
+		
+		System.out.println("Solution: " + solution.maxLength(input));
+		
 		
 	}
 }
